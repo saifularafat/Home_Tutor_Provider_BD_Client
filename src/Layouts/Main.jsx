7c23b483b45755be37a076db2bc820e271348546
saifularafat <saifularafat.info@gmail.com>
2024-10-09 23:27:52 +0600

    Error page Done

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
         <div>
             <NavBer />
             <p>Lorem ipsum dolor sit amet.</p>
-            <div className="min-h-[calc(100vh-0px)]">
+            <div className="min-h-[calc(100vh-564px)]">
                 <Outlet />
             </div>
             <Footer />
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,10 +1,20 @@
-
+import Lottie from "lottie-react";
+import errorLottie from "./../../assets/Animation/error.json"
+import "./../../Share/Footer/footer.css"
+import { Link } from "react-router-dom";
 const Error = () => {
-    return (
-        <div>
-           <p>Error page</p> 
+return (
+    <div className="footerBg h-screen">
+        <div className="w-5/12 mx-auto">
+            <Lottie
+                animationData={errorLottie}
+                loop={true} className="" />
         </div>
-    );
+        <div className="text-center">
+            <Link to="/" className=" bg-orange-500 py-2 px-5 rounded-lg text-lg font-medium text-white  hover:bg-orange-600 transition-all duration-200">Go Home</Link>
+        </div>
+    </div>
+);
 };
 
 export default Error;
