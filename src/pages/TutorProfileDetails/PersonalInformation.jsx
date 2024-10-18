f6cdd08ebce1ec668ccbf97c172eb8429deb0f34
saifularafat <saifularafat.info@gmail.com>
2024-10-19 03:40:38 +0600

    tutor profile details is design compelete

--- src/pages/TutorProfileDetails/PersonalInformation.jsx
+++ src/pages/TutorProfileDetails/PersonalInformation.jsx
@@ -1,9 +1,31 @@
-  const PersonalInformation = () => {
-    return (
-        <div>
-            
+const PersonalInformation = () => {
+  return (
+    <div className="pt-7 pb-2">
+      <h3 className="text-2xl font-bold text-[#165b81]">Personal Information</h3>
+      <div className="flex justify-between gap-5">
+        <div className="w-1/2 pt-4">
+          <p><span className="text-base font-semibold text-slate-700">Gender: </span>Male</p>
         </div>
-    );
- };
- 
- export default PersonalInformation;
+        <div className="w-1/2 pt-4">
+          <p><span className="text-base font-semibold text-slate-700">
+            Living Country: </span>Bangladesh</p>
+        </div>
+      </div>
+      <div className="flex justify-between gap-5">
+        <div className="w-1/2 pt-4">
+          <p><span className="text-base font-semibold text-slate-700">Living City: </span>Dhaka</p>
+        </div>
+        <div className="w-1/2 pt-4">
+          <p><span className="text-base font-semibold text-slate-700">Living Location: </span>Mirpur</p>
+        </div>
+      </div>
+      <div className="flex justify-between gap-5">
+        <div className="w-1/2 pt-4">
+          <p><span className="text-base font-semibold text-slate-700">Member Since: </span> 1 hour ago</p>
+        </div>
+      </div>
+    </div >
+  );
+};
+
+export default PersonalInformation;
--- src/pages/TutorProfileDetails/PreferredTutoringClasses_Courses.jsx
+++ src/pages/TutorProfileDetails/PreferredTutoringClasses_Courses.jsx
@@ -1,8 +1,50 @@
 
 const PreferredTutoringClasses_Courses = () => {
     return (
-        <div>
-            
+        <div className="pt-8">
+            <h2 className="text-2xl font-bold text-[#165B81]">Preferred Tutoring Classes & Courses</h2>
+
+
+            {/* educational table information */}
+
+
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg pt-4">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+                        <tr className="bg-blue-200">
+                            <th scope="col" className="px-6 py-3 text-base font-semibold capitalize">
+                                Class
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-base font-semibold capitalize">
+                                Medium
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-base font-semibold capitalize">
+                                Subjects
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-base font-semibold capitalize">
+                                Salary
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                                1 to 8
+                            </th>
+                            <td className="px-6 py-4 font-medium">
+                                Bangla Medium
+                            </td>
+                            <td className="px-6 py-4 font-medium bg-gray-100 dark:bg-gray-800 ">
+                                Accounting
+                            </td>
+                            <td className="px-6 py-4 font-medium">
+                                5000
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
