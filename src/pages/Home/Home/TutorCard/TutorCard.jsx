88824ae66d91068c4a84dbea86c94f5402d2e25c
saifularafat <saifularafat.info@gmail.com>
2024-10-25 03:40:26 +0600

    design is compelete

--- src/pages/Home/Home/TutorCard/TutorCard.jsx
+++ src/pages/Home/Home/TutorCard/TutorCard.jsx
@@ -11,36 +11,38 @@ import { Link, useParams } from 'react-router-dom'
 const TutorCard = () => {
     const { id } = useParams()
     return (
-        <div className='p-5 my-20 '>
-            <Swiper
-                freeMode={true}
-                slidesPerView={1}
-                spaceBetween={10}
-                pagination={{
-                    clickable: true
-                }}
-                breakpoints={{
-                    540: {
-                        slidesPerView: 2,
-                        spaceBetween: 20
-                    },
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 30
-                    },
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 50
-                    },
-                    1270: {
-                        slidesPerView: 4,
-                        spaceBetween: 50
-                    }
-                }}
-                modules={[FreeMode, Pagination]}
-                className='mySwiper'
-            >
-                {/* <SwiperSlide>
+        <div className='container mx-auto'>
+            <div className='p-5 my-10 '>
+                <h2 className='text-3xl font-bold text-slate-700 text-center pb-12'>All Tutors</h2>
+                <Swiper
+                    freeMode={true}
+                    slidesPerView={1}
+                    spaceBetween={10}
+                    pagination={{
+                        clickable: true
+                    }}
+                    breakpoints={{
+                        540: {
+                            slidesPerView: 2,
+                            spaceBetween: 20
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 30
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 50
+                        },
+                        1270: {
+                            slidesPerView: 4,
+                            spaceBetween: 50
+                        }
+                    }}
+                    modules={[FreeMode, Pagination]}
+                    className='mySwiper'
+                >
+                    {/* <SwiperSlide>
                     <div className="shadow-xl border  card w-96 -z-10 h-80 mb-7">
                         <figure className="pt-5 pb-2">
                             <img
@@ -62,58 +64,159 @@ const TutorCard = () => {
                         </div>
                     </div>
                 </SwiperSlide> */}
-               
-                <SwiperSlide>
-                    <div className="shadow-xl border flex flex-col w-96 h-80 mb-7 p-5">
-                        <div className="flex justify-center">
-                            <img
-                                width={110}
-                                height={110}
-                                className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
-                                src={user1} alt="Tutor photo" />
-                        </div>
-                        <div className="text-center flex-grow">
-                            <h1 className="text-2xl text-gray-700 font-bold">Nullify</h1>
-                            <div className='flex items-center justify-center gap-2'>
-                                <FaLocationDot className='text-slate-500' />
-                                <p className='text-base font-medium text-slate-600'>Dhaka</p>
+
+                    <SwiperSlide>
+                        <div className="shadow-lg border border-slate-200 hover:shadow-2xl hover:shadow-slate-300 rounded-xl flex flex-col space-y-2 w-96 h-80 mb-7 p-5">
+                            <div className="flex justify-center">
+                                <img
+                                    width={110}
+                                    height={110}
+                                    className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
+                                    src={user1} alt="Tutor photo" />
+                            </div>
+                            <div className="text-center flex-grow space-y-2">
+                                <h1 className="text-2xl text-gray-700 font-bold capitalize">Nullify ullify</h1>
+                                <div className='flex items-center justify-center gap-2'>
+                                    <FaLocationDot className='text-slate-500' />
+                                    <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                                </div>
+                            </div>
+                            <div className="mt-auto flex justify-center">
+                                <Link
+                                    to={`/tutor-details/${id}`}
+                                    className="rounded-full tracking-wide py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
+                                    View Profile
+                                </Link>
                             </div>
                         </div>
-                        <div className="mt-auto flex justify-center">
-                            <Link
-                                to={`/tutor-details/${id}`}
-                                className="rounded-full py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
-                                View Profile
-                            </Link>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="shadow-xl border flex flex-col w-96 h-80 mb-7 p-5">
-                        <div className="flex justify-center">
-                            <img
-                                width={110}
-                                height={110}
-                                className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
-                                src={user1} alt="Tutor photo" />
-                        </div>
-                        <div className="text-center flex-grow">
-                            <h1 className="text-2xl text-gray-700 font-bold">Nullify</h1>
-                            <div className='flex items-center justify-center gap-2'>
-                                <FaLocationDot className='text-slate-500' />
-                                <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="shadow-lg border border-slate-200 hover:shadow-2xl hover:shadow-slate-300 rounded-xl flex flex-col space-y-2 w-96 h-80 mb-7 p-5">
+                            <div className="flex justify-center">
+                                <img
+                                    width={110}
+                                    height={110}
+                                    className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
+                                    src={user1} alt="Tutor photo" />
+                            </div>
+                            <div className="text-center flex-grow space-y-2">
+                                <h1 className="text-2xl text-gray-700 font-bold capitalize">Nullify ullify</h1>
+                                <div className='flex items-center justify-center gap-2'>
+                                    <FaLocationDot className='text-slate-500' />
+                                    <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                                </div>
+                            </div>
+                            <div className="mt-auto flex justify-center">
+                                <Link
+                                    to={`/tutor-details/${id}`}
+                                    className="rounded-full tracking-wide py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
+                                    View Profile
+                                </Link>
                             </div>
                         </div>
-                        <div className="mt-auto flex justify-center">
-                            <Link
-                                to={`/tutor-details/${id}`}
-                                className="rounded-full py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
-                                View Profile
-                            </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="shadow-lg border border-slate-200 hover:shadow-2xl hover:shadow-slate-300 rounded-xl flex flex-col space-y-2 w-96 h-80 mb-7 p-5">
+                            <div className="flex justify-center">
+                                <img
+                                    width={110}
+                                    height={110}
+                                    className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
+                                    src={user1} alt="Tutor photo" />
+                            </div>
+                            <div className="text-center flex-grow space-y-2">
+                                <h1 className="text-2xl text-gray-700 font-bold capitalize">Nullify ullify</h1>
+                                <div className='flex items-center justify-center gap-2'>
+                                    <FaLocationDot className='text-slate-500' />
+                                    <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                                </div>
+                            </div>
+                            <div className="mt-auto flex justify-center">
+                                <Link
+                                    to={`/tutor-details/${id}`}
+                                    className="rounded-full tracking-wide py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
+                                    View Profile
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="shadow-lg border border-slate-200 hover:shadow-2xl hover:shadow-slate-300 rounded-xl flex flex-col space-y-2 w-96 h-80 mb-7 p-5">
+                            <div className="flex justify-center">
+                                <img
+                                    width={110}
+                                    height={110}
+                                    className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
+                                    src={user1} alt="Tutor photo" />
+                            </div>
+                            <div className="text-center flex-grow space-y-2">
+                                <h1 className="text-2xl text-gray-700 font-bold capitalize">Nullify ullify</h1>
+                                <div className='flex items-center justify-center gap-2'>
+                                    <FaLocationDot className='text-slate-500' />
+                                    <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                                </div>
+                            </div>
+                            <div className="mt-auto flex justify-center">
+                                <Link
+                                    to={`/tutor-details/${id}`}
+                                    className="rounded-full tracking-wide py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
+                                    View Profile
+                                </Link>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="shadow-lg border border-slate-200 hover:shadow-2xl hover:shadow-slate-300 rounded-xl flex flex-col space-y-2 w-96 h-80 mb-7 p-5">
+                            <div className="flex justify-center">
+                                <img
+                                    width={110}
+                                    height={110}
+                                    className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
+                                    src={user1} alt="Tutor photo" />
+                            </div>
+                            <div className="text-center flex-grow space-y-2">
+                                <h1 className="text-2xl text-gray-700 font-bold capitalize">Nullify ullify</h1>
+                                <div className='flex items-center justify-center gap-2'>
+                                    <FaLocationDot className='text-slate-500' />
+                                    <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                                </div>
+                            </div>
+                            <div className="mt-auto flex justify-center">
+                                <Link
+                                    to={`/tutor-details/${id}`}
+                                    className="rounded-full tracking-wide py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
+                                    View Profile
+                                </Link>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="shadow-lg border border-slate-200 hover:shadow-2xl hover:shadow-slate-300 rounded-xl flex flex-col space-y-2 w-96 h-80 mb-7 p-5">
+                            <div className="flex justify-center">
+                                <img
+                                    width={110}
+                                    height={110}
+                                    className="h-[120px] w-[120px] rounded-full bg-slate-500 object-cover"
+                                    src={user1} alt="Tutor photo" />
+                            </div>
+                            <div className="text-center flex-grow space-y-2">
+                                <h1 className="text-2xl text-gray-700 font-bold capitalize">Nullify ullify</h1>
+                                <div className='flex items-center justify-center gap-2'>
+                                    <FaLocationDot className='text-slate-500' />
+                                    <p className='text-base font-medium text-slate-600'>Dhaka</p>
+                                </div>
+                            </div>
+                            <div className="mt-auto flex justify-center">
+                                <Link
+                                    to={`/tutor-details/${id}`}
+                                    className="rounded-full tracking-wide py-2 px-4 text-[12px] font-medium text-sky-700 ring-1 ring-sky-700 hover:bg-sky-700 hover:text-white sm:text-sm transition-all duration-300">
+                                    View Profile
+                                </Link>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     );
 };
