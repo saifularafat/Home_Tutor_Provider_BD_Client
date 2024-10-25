502b7e7121f976023fa30b0e4f95b3002e34a2c7
saifularafat <saifularafat.info@gmail.com>
2024-10-26 00:03:42 +0600

    section title is devided others component

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,7 @@
+const SectionTitle = ({ sectionName }) => {
+    return (
+        <h2 className="text-3xl font-bold text-center text-slate-800 py-5">{sectionName}</h2>
+    );
+};
+
+export default SectionTitle;
