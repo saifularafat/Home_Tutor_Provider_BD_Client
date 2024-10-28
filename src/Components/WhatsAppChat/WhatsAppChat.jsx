004ab4dd6fbc299e2c6d7d02a2d844b120de34d3
saifularafat <saifularafat.info@gmail.com>
2024-10-28 12:38:50 +0600

    about page a content change in this website

--- src/Components/WhatsAppChat/WhatsAppChat.jsx
+++ src/Components/WhatsAppChat/WhatsAppChat.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const WhatsAppChat = () => {
     const [openModal, setOpenModal] = useState(false);
     return (
-        <div className='z-50'>
+        <div className='z-[999999999px]'>
             <div className='fixed -right-2 bottom-1 md:bottom-5 md:right-4'>
                 <button onClick={() => setOpenModal(true)} className="rounded-tr-3xl rounded-tl-3xl rounded-br-3xl bg-slate-50">
                     <FaWhatsapp
@@ -13,15 +13,15 @@ const WhatsAppChat = () => {
                         alt=''
                     />
                 </button>
-                <div onClick={() => setOpenModal(false)} className={`fixed z-[100] w-screen ${openModal ? 'visible opacity-100' : 'invisible opacity-0'} inset-0 grid place-items-center bg-black/20 backdrop-blur-sm duration-100 dark:bg-transparent`}>
+                <div onClick={() => setOpenModal(false)} className={`fixed z-[999999999px] w-screen ${openModal ? 'visible opacity-100 z-[999999999px]' : 'invisible opacity-0 z-[999999999px]'} inset-0 grid place-items-center bg-black/20 backdrop-blur-sm duration-100 dark:bg-transparent`}>
                     <div onClick={(e_) => e_.stopPropagation()}
-                        className={`absolute right-0 translate-y-1/2 mr-5 max-w-md rounded-lg bg-white p-6 drop-shadow-lg dark:bg-zinc-900 dark:text-white ${openModal ? 'opacity-1 duration-300' : 'scale-110 opacity-0 duration-150'}`}
+                        className={`absolute right-0 translate-y-1/2 mr-5 max-w-md rounded-lg bg-white md:p-6 p-3 drop-shadow-lg dark:bg-zinc-900 dark:text-white ${openModal ? 'opacity-1 duration-300' : 'scale-110 opacity-0 duration-150'}`}
                     >
                         <svg onClick={() => setOpenModal(false)} className="absolute right-3 top-3 w-6 cursor-pointer fill-zinc-600 dark:fill-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"></path></svg>
-                        <div className="text-center">
+                        <div className="text-center z-[999999999px]">
                             <p>Welcome to </p>
-                            <h1 className="mb-2 text-2xl font-semibold">H-TutorProvider<sup className="text-green-400">B<span className="text-red-400">D</span></sup></h1>
-                            <p className="mb-5 text-sm opacity-80">Elevate your React projects with beautifully crafted components designed for TailwindCSS.</p>
+                            <h1 className="md:mb-2 mb-1 md:text-2xl text-xl font-semibold">H-TutorProvider<sup className="text-green-400">B<span className="text-red-400">D</span></sup></h1>
+                            <p className="md:mb-5 mb-3 text-sm opacity-80">Elevate your React projects with beautifully crafted components designed for TailwindCSS.</p>
                             <Link to="">
                                 <FaWhatsapp
                                     className='transition-all w-8 h-8 md:h-16 md:w-16 text-[#25D147]'
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,38 +1,113 @@
+import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import PageTitleShow from '../../Components/PageTitleShow/PageTitleShow';
 
-import Lottie from "lottie-react";
-import aboutLottie from "./../../assets/Animation/about.json"
-import WhyChooseUs from "../Home/Home/WhyChooseUs/WhyChooseUs";
 const About = () => {
     return (
-        <>
-            <div className=" footerBg ">
-                <div className="grid grid-cols-4 gap-5 w-10/12 mx-auto py-10 ">
-                    <div className="col-span-2 w-full p-5 hover:shadow-lg rounded-md transition-all duration-200">
-                        <Lottie
-                            animationData={aboutLottie}
-                            loop={true} className="" />
-                    </div>
-                    <div className="col-span-2 hover:shadow-lg hover:rounded-md transition-all duration-200 p-5">
-                        <h3 className="text-3xl font-semibold text-slate-800 pb-4">About Us</h3>
-                        <p className="text-justify text-[18px] text-slate-700">
-                            At <span className="text-lg font-semibold text-orange-600">Home Tutor Provider BD</span>, we are dedicated to revolutionizing education through personalized learning experiences.
-                            Our mission is to connect students with exceptional tutors who can guide them towards achieving their academic goals and unlocking their full potential.
-                        </p>
-                        <p className="text-justify  text-[18px] text-slate-700 py-5">
-                            With a team of passionate educators and a commitment to excellence, we strive to create a supportive and engaging learning environment.
-                            Our platform offers a range of flexible options to meet the diverse needs of learners, ensuring that every student receives the attention and resources they need to succeed.
-                        </p>
-                        <p className="text-justify  text-[18px] text-slate-700">
-                            Join us on our journey to make quality education accessible to all. Together, we can make a difference in the lives
-                            of students and shape the future of education.
-                        </p>
+        <div className='container px-6 mx-auto'>
+            <PageTitleShow currentPage="About us" />
+
+            <div className='overflow-x-hidden items-center justify-between py-20 text-center md:flex md:gap-20 md:text-left'>
+                <div className=' md:w-1/2'>
+                    <h2
+                        data-aos='zoom-in'
+                        data-aos-duration='1000'
+                        className='pb-3 text-sm'
+                    >
+                        About Home Tutor Provider <sup>BD</sup>
+                    </h2>
+                    <div
+                        data-aos='fade-right'
+                        data-aos-easing='ease-in-sine'
+                        className='pb-5'
+                    >
+                        <h2 className='pb-3 text-4xl font-bold tracking-wide xl:text-7xl'>
+                            Reach your peak.
+                        </h2>
+                        <h2 className='text-4xl font-bold tracking-wide xl:text-7xl'>
+                            We'll help you get there.
+                        </h2>
                     </div>
 
+                    <p
+                        data-aos='fade-right'
+                        data-aos-easing='ease-in-sine'
+                        className='pb-5 text-lg  '
+                    >
+                        We recognize the importance of seamless and efficient examination
+                        processes, and that's why we have created a cutting-edge solution
+                        that allows students to take their exams from the comfort of their
+                        own space. Our platform is built on a foundation of innovation and
+                        user-centric design, ensuring a user-friendly experience.
+                    </p>
+                    <Link
+                        data-aos='fade-down'
+                        data-aos-duration='1000'
+                        to='/contact'
+                        className='  border-none shadow-md btn btn-primary'
+                    >
+                        Contact Us
+                    </Link>
+                </div>
+
+                <div
+                    data-aos='fade-left'
+                    data-aos-easing='ease-in-sine'
+                    className='md:w-1/2'
+                >
+                    <img
+                        className='w-2/3 mx-auto mt-10'
+                        src='https://i.ibb.co/sPdHD97/45397-removebg.png'
+                        alt=''
+                    />
                 </div>
             </div>
-            <WhyChooseUs />
-        </>
-    );
-};
+
+            
+
+          
+        </div>
+    )
+}
 
 export default About;
+
+
+
+
+
+// import Lottie from "lottie-react";
+// import aboutLottie from "./../../assets/Animation/about.json"
+// const About = () => {
+//     return (
+//         <>
+//             <div className=" footerBg ">
+//                 <div className="grid grid-cols-4 gap-5 w-10/12 mx-auto py-10 ">
+//                     <div className="col-span-2 w-full p-5 hover:shadow-lg rounded-md transition-all duration-200">
+//                         <Lottie
+//                             animationData={aboutLottie}
+//                             loop={true} className="" />
+//                     </div>
+//                     <div className="col-span-2 hover:shadow-lg hover:rounded-md transition-all duration-200 p-5">
+//                         <h3 className="text-3xl font-semibold text-slate-800 pb-4">About Us</h3>
+//                         <p className="text-justify text-[18px] text-slate-700">
+//                             At <span className="text-lg font-semibold text-orange-600">Home Tutor Provider BD</span>, we are dedicated to revolutionizing education through personalized learning experiences.
+//                             Our mission is to connect students with exceptional tutors who can guide them towards achieving their academic goals and unlocking their full potential.
+//                         </p>
+//                         <p className="text-justify  text-[18px] text-slate-700 py-5">
+//                             With a team of passionate educators and a commitment to excellence, we strive to create a supportive and engaging learning environment.
+//                             Our platform offers a range of flexible options to meet the diverse needs of learners, ensuring that every student receives the attention and resources they need to succeed.
+//                         </p>
+//                         <p className="text-justify  text-[18px] text-slate-700">
+//                             Join us on our journey to make quality education accessible to all. Together, we can make a difference in the lives
+//                             of students and shape the future of education.
+//                         </p>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default About;
