771231a6a6f8baa047c7a1270d330c8ed3b0aa8c
saifularafat <saifularafat.info@gmail.com>
2024-11-04 12:23:36 +0600

    registration view page is resppnsive

--- src/pages/RegistrationView/RegistrationView.jsx
+++ src/pages/RegistrationView/RegistrationView.jsx
@@ -14,17 +14,17 @@ const RegistrationView = () => {
     return (
         <div className="container mx-auto py-12">
             <h3 className="text-4xl font-extrabold tracking-wider text-slate-800 text-center">Join Us Today</h3>
-            <div className='grid grid-cols-3 gap-5 items-center py-12'>
+            <div className='grid md:grid-cols-3 grid-cols-1 mx-3 md:mx-0 space-y-5 md:gap-5 items-center md:py-12 py-1'>
                 <label
-                    className={`cursor-pointer grid grid-cols-2 hover:scale-105 transition-all duration-200 md:w-[400px] rounded-lg
+                    className={`cursor-pointer grid md:grid-cols-2 grid-cols-1 hover:scale-105 transition-all duration-200 md:w-[400px] rounded-lg mt-5
           ${selectedOption === 'parent' ? 'bg-[#1e5e35] ' : 'bg-[#16A34A]'}
           `}
                 >
-                    <div className="md:w-[400px] shadow-md hover:shadow-2xl hover:shadow-sky-300">
+                    <div className="md:w-[400px] w-full h-full shadow-xl hover:shadow-sky-300 md:mb-0 mb-5">
                         <div className="pt-5">
                             <FaUsers className="text-white w-12 h-12 mx-auto mb-6" />
                         </div>
-                        <div className="text-center bg-white z-10 rounded-lg w-full h-36 p-7">
+                        <div className="text-center bg-white z-30 h-2/3 rounded-lg w-full md:p-7 p-3">
                             <h1 className="text-2xl text-[#16A34A] font-bold pb-3">Parent Registration</h1>
                             <p className="font-medium text-slate-600">Register as a parent to find a tutor for your child.</p>
                         </div>
@@ -39,15 +39,15 @@ const RegistrationView = () => {
                     />
                 </label>
                 <label
-                    className={`cursor-pointer grid grid-cols-2 hover:scale-105 transition-all duration-200 md:w-[400px] rounded-lg
+                    className={`cursor-pointer grid md:grid-cols-2 grid-cols-1 hover:scale-105 transition-all duration-200 md:w-[400px] rounded-lg
            ${selectedOption === 'tutor' ? ' bg-sky-600' : 'bg-sky-400'
                         }
            `} >
-                    <div className="md:w-[400px] shadow-md hover:shadow-2xl hover:shadow-sky-300 ">
+                    <div className="md:w-[400px] w-full h-full shadow-xl hover:shadow-sky-300 mb-5">
                         <div className="pt-5">
                             <FaUserGraduate className="text-white w-12 h-12 mx-auto mb-6" />
                         </div>
-                        <div className="text-center bg-white z-10 rounded-lg w-full h-36 p-7">
+                        <div className="text-center bg-white z-30 h-2/3 rounded-lg w-full md:p-7 p-3">
                             <h1 className="text-2xl text-sky-600 font-bold pb-3">Tutor Registration</h1>
                             <p className="font-medium text-slate-600">Sign up as a tutor to start teaching.</p>
                         </div>
@@ -62,17 +62,17 @@ const RegistrationView = () => {
                     />
                 </label>
                 <label
-                    className={`cursor-pointer grid grid-cols-2 hover:scale-105 transition-all duration-200 md:w-[400px] rounded-lg
+                    className={`cursor-pointer grid md:grid-cols-2 grid-cols-1 hover:scale-105 transition-all duration-200 md:w-[400px] rounded-lg
            ${selectedOption === 'coaching' ? ' bg-red-600' : 'bg-red-400'
                         }
            
            `}
                 >
-                    <div className="md:w-[400px] shadow-md hover:shadow-2xl hover:shadow-red-300 ">
+                    <div className="md:w-[400px] w-full h-full shadow-xl hover:shadow-red-300 md:mb-0 mb-5">
                         <div className="pt-5">
                             <GiFamilyHouse className="text-white w-12 h-12 mx-auto mb-6" />
                         </div>
-                        <div className="text-center bg-white z-10 rounded-lg w-full p-7">
+                        <div className="text-center bg-white z-30 h-2/3 rounded-lg w-full md:p-7 p-3">
                             <h1 className="text-2xl text-red-500 font-bold pb-3">Coaching Registration</h1>
                             <p className="font-medium text-slate-600">Sign up your coaching center to start accepting students.</p>
                         </div>
@@ -87,15 +87,15 @@ const RegistrationView = () => {
                     />
                 </label>
             </div>
-            <div className="text-center text-lg ">
+            <div className="text-center text-lg md:pt-0 pt-6">
                 {
                     selectedOption ?
                         <Link to={`/registration/${selectedOption}`}
                             state={{ registerRole: selectedOption }}>
-                            <button className="group relative z-20 h-14 w-32 overflow-hidden border-y-4 border-sky-950 bg-sky-700 text-xl text-white duration-500"><span className="uppercase">{selectedOption ? selectedOption : "User"}</span><span className="absolute inset-0 z-10 flex items-center justify-center text-white opacity-0 duration-100 ease-out group-hover:opacity-100 group-hover:duration-1000">Register</span><span className="absolute inset-0 -translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span><span className="absolute inset-0 -translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span></button>
+                            <button className="group relative z-20 h-14 w-32 overflow-hidden border-y-4 border-sky-950 bg-sky-700 md:text-xl text-base tracking-wider text-white duration-500"><span className="uppercase">{selectedOption ? selectedOption : "User"}</span><span className="absolute inset-0 z-10 flex items-center justify-center text-white opacity-0 duration-100 ease-out group-hover:opacity-100 group-hover:duration-1000">Register</span><span className="absolute inset-0 -translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span><span className="absolute inset-0 -translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span></button>
                         </Link>
                         :
-                        <button disabled className="group relative z-20 h-14 w-32 overflow-hidden border-y-4 border-sky-950 bg-sky-700 text-xl text-white duration-500"><span className="uppercase">{selectedOption ? selectedOption : "User"}</span><span className="absolute inset-0 z-10 flex items-center justify-center text-white opacity-0 duration-100 ease-out group-hover:opacity-100 group-hover:duration-1000">Register</span><span className="absolute inset-0 -translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span><span className="absolute inset-0 -translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span></button>
+                        <button disabled className="group relative z-20 h-14 w-32 overflow-hidden border-y-4 border-sky-950 bg-sky-700 md:text-xl text-base tracking-wider text-white duration-500"><span className="uppercase">{selectedOption ? selectedOption : "User"}</span><span className="absolute inset-0 z-10 flex items-center justify-center text-white opacity-0 duration-100 ease-out group-hover:opacity-100 group-hover:duration-1000">Register</span><span className="absolute inset-0 -translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-y-full bg-sky-950 group-hover:translate-y-0 group-hover:duration-1000"></span><span className="absolute inset-0 translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span><span className="absolute inset-0 -translate-x-full bg-sky-950 delay-100 duration-1000 group-hover:translate-x-0 group-hover:delay-300"></span></button>
                 }
             </div>
         </div>
