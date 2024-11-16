5d365f502675b2ca733a585b9e06c636fd3bd058
saifularafat <saifularafat.info@gmail.com>
2024-11-16 23:56:17 +0600

    all Payment padding and margin set

--- src/pages/Deashboard/Admin/AllPayments/AllPayments/AllPayments.jsx
+++ src/pages/Deashboard/Admin/AllPayments/AllPayments/AllPayments.jsx
@@ -28,39 +28,42 @@ const AllPayments = () => {
     ]
 
     return (
-        <div>
+        <div className="py-5">
             {/* TODO TOTAL AMOUNT */}
-            <h3 className="md:text-left pl-2 md:pl-0 text-lg md:text-xl font-semibold md:pb-5 pb-2 uppercase"><span className="md:text-3xl text-2xl">
+            <h3 className="md:text-left text-slate-600 pl-2 md:pl-8 text-lg md:text-xl font-bold md:pb-3 pb-2 uppercase">
+                {/* <span className="md:text-3xl text-2xl text-center"> */}
                 {/* {totalAmount + "$"} */}
-            </span> TOTAL Amount <span className="text-xs">
-                    {/* (pending pay: {totalPAmount + "$"}) */}
-                </span>
+                {/* </span>  */}
+                TOTAL Amount
+                {/* <span className="text-xs"> */}
+                {/* (pending pay: {totalPAmount + "$"}) */}
+                {/* </span> */}
             </h3>
 
             <div className="overflow-x-auto ">
-                <table className="min-w-[95%] mx-auto shadow-lg my-6">
+                <table className="md:min-w-[95%] mx-auto shadow-lg mb-6">
                     <thead>
                         <tr className="bg-[#126faa] text-white">
-                            <th className="py-2 px-2 text-center text-base font-medium">##</th>
-                            <td className="py-2 px-2 text-center text-base font-medium">Email</td>
-                            <td className="py-2 px-2 text-center text-base font-medium">User Name</td>
-                            <td className="py-2 px-2 text-center text-base font-medium">Pay Type</td>
-                            <td className="py-2 px-2 text-center text-base font-medium">Tuition Free</td>
-                            <td className="py-2 px-2 text-center text-base font-medium">Transition ID</td>
-                            <td className="py-2 px-2 text-center text-base font-medium">Date</td>
+                            <th className="md:py-2 md:px-2 text-center text-base font-medium">##</th>
+                            <td className="md:py-2 md:px-2 text-center text-base font-medium">Email</td>
+                            <td className="md:py-2 md:px-2 text-center text-base font-medium">User Name</td>
+                            <td className="md:py-2 md:px-2 text-center text-base font-medium">Pay Type</td>
+                            <td className="md:py-2 md:px-2 text-center text-base font-medium">Tuition Free</td>
+                            <td className="md:py-2 md:px-2 text-center text-base font-medium">Transition ID</td>
+                            <td className="md:py-2 md:px-2 text-center text-base font-medium">Date</td>
                         </tr>
                     </thead>
                     <tbody className="bg-white">
                         {
                             displayedData.map((pay, index) =>
                                 <tr key={index} className="">
-                                    <td className="text-center py-1">{index + 1}</td>
-                                    <td className="text-center py-1">{pay?.email}</td>
-                                    <td className="text-center py-1 capitalize">{pay?.name}</td>
-                                    <td className={`text-center py-1 font-medium ${pay?.paymentType === 'bkash' ? 'bg-[#CA1F50] text-white': 'bg-[#E98F23] text-white'}`}>{pay?.paymentType}</td>
-                                    <td className="text-center py-1">{pay?.tuitionFree} <span className="text-xl font-bold">৳</span></td>
-                                    <td className="text-center py-1">{pay?.transition_id}</td>
-                                    <td className="text-center py-1">{pay?.date}</td>
+                                    <td className="text-center md:py-1">{index + 1}</td>
+                                    <td className="text-center md:py-1">{pay?.email}</td>
+                                    <td className="text-center md:py-1 capitalize">{pay?.name}</td>
+                                    <td className={`text-center md:py-1 font-medium ${pay?.paymentType === 'bkash' ? 'bg-[#CA1F50] text-white' : 'bg-[#E98F23] text-white'}`}>{pay?.paymentType}</td>
+                                    <td className="text-center md:py-1">{pay?.tuitionFree} <span className="text-xl font-bold">৳</span></td>
+                                    <td className="text-center md:py-1">{pay?.transition_id}</td>
+                                    <td className="text-center md:py-1">{pay?.date}</td>
                                 </tr>
                             )
                         }
