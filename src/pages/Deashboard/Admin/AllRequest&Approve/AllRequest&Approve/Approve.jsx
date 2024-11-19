ba01b242c6bd7f7beecd4402fccb52edfe0024e0
saifularafat <saifularafat.info@gmail.com>
2024-11-19 22:20:29 +0600

    request and approve page teger design complete

--- src/pages/Deashboard/Admin/AllRequest&Approve/AllRequest&Approve/Approve.jsx
+++ src/pages/Deashboard/Admin/AllRequest&Approve/AllRequest&Approve/Approve.jsx
@@ -3,34 +3,62 @@ const Approve = () => {
     return (
 
         <div className="overflow-x-auto">
-            <table className="min-w-[90%] shadow-md border mx-auto border-gray-100 my-6">
+            <table className="min-w-[100%] shadow-md border mx-auto border-gray-100">
                 <thead>
-                    <tr className="bg-[#0095FF] text-white">
-                        <th className="py-4 px-6 text-lg text-left border-b">Product Image</th>
-                        <th className="py-4 px-6 text-lg text-left border-b">Product Name</th>
-                        <th className="py-4 px-6 text-lg text-left border-b">Price</th>
-                        <th className="py-4 px-6 text-lg border-b text-end">Action</th>
+                    <tr className="bg-blue-500 text-white">
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">tutor name</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">T. Id</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">T. Area</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Subject</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Class</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Medium</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Salary</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Per Week</th>
+                        <th className="py-2 pr-2 border-b capitalize text-sm font-semibold text-center">Job Area</th>
+                        <th className="py-2 pr-2 border-b capitalize text-sm font-semibold text-center">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="hover:bg-gray-50 border-b transition duration-300">
-                        <td className="py-4 px-4 flex justify-start">
-                            <img src="https://source.unsplash.com/64x64/?microphone" alt="table navigate ui" className="h-16 w-16 object-cover bg-gray-300" />
-                        </td>
-                        <td className="py-4 px-6 border-b text-xl font-medium">Dual Speaker</td>
-                        <td className="py-4 px-6 border-b text-lg font-medium">$99.99</td>
-                        <td className="py-4 px-6 border-b text-end">
-                            <button className="bg-blue-500 hover:scale-110 scale-100 transition-all duration-100 text-white py-2 px-4 rounded-md">Details</button>
+                    <tr className="hover:bg-gray-100 transition duration-300">
+                        <td className="p-1 text-xs font-medium text-center border-b">Shiyam Ahmed</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">HPT-19021</td>
+                        <td className="p-1 text-xs font-medium border-b text-end">Mirpur 15, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Sciences</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">class- 9 to 10</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Bangla</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">5K</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">4d</td>
+                        <td className="p-1 text-xs font-medium border-b text-end"> Shemoley, Mirpur-10, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">
+                            <button className="text-[11px] px-1 py-1 bg-red-500 text-white rounded-lg">Delete</button>
                         </td>
                     </tr>
-                    <tr className="hover:bg-gray-50 border-b transition duration-300">
-                        <td className="py-4 px-4 flex justify-start">
-                            <img src="https://source.unsplash.com/64x64/?phone" alt="table navigate ui" className="h-16 w-16 object-cover bg-gray-300" />
+                    <tr className="hover:bg-gray-100 transition duration-300">
+                        <td className="p-1 text-xs font-medium text-center border-b">Parvej Ahmed Anto</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">HPT-19023</td>
+                        <td className="p-1 text-xs font-medium border-b text-end">Uttora-1, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Commerces</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">class- Ten</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Bangla</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">4K</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">3d</td>
+                        <td className="p-1 text-xs font-medium border-b text-end"> Uttora-2, Ges Fames, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">
+                            <button className="text-[11px] px-1 py-1 bg-red-500 text-white rounded-lg">Delete</button>
                         </td>
-                        <td className="py-4 px-6 border-b text-xl font-medium">Samsung s22</td>
-                        <td className="py-4 px-6 border-b text-lg font-medium">$599.99</td>
-                        <td className="py-4 px-6 border-b text-end">
-                            <button className="bg-blue-500 hover:scale-110 scale-100 transition-all duration-100 text-white py-2 px-4 rounded-md">Details</button>
+                    </tr>
+                    <tr className="hover:bg-gray-100 transition duration-300">
+                        <td className="p-1 text-xs font-medium text-center border-b">Tanjila Akter Shemo</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">HPT-19026</td>
+                        <td className="p-1 text-xs font-medium border-b text-end">Danmondi, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Sciences</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">class- 11 to 12</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">English</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">7K</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">4d</td>
+                        <td className="p-1 text-xs font-medium border-b text-end"> Fukira Bazar, Mirpur-10, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">
+                            <button className="text-[11px] px-1 py-1 bg-red-500 text-white rounded-lg">Delete</button>
                         </td>
                     </tr>
                 </tbody>
--- src/pages/Deashboard/Admin/AllRequest&Approve/AllRequest&Approve/Request.jsx
+++ src/pages/Deashboard/Admin/AllRequest&Approve/AllRequest&Approve/Request.jsx
@@ -5,25 +5,64 @@ const Request = () => {
         <div className="overflow-x-auto">
             <table className="min-w-[100%] shadow-md  border mx-auto border-gray-100">
                 <thead>
-                    <tr className="bg-[#333333] text-white">
-                        <th className="py-3 px-6 text-left border-b">Name</th>
-                        <th className="py-3 px-6 text-left border-b">Age</th>
-                        <th className="py-3 px-6 text-left border-b">Gender</th>
-                        <th className="py-3 px-6  border-b text-end">Address</th>
+                    <tr className="bg-[#403f3f] text-white">
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">tutor name</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">T. Id</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">T. Area</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Subject</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Class</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Medium</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Salary</th>
+                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Per Week</th>
+                        <th className="py-2 pr-2 border-b capitalize text-sm font-semibold text-center">Job Area</th>
+                        <th className="py-2 pr-2 border-b capitalize text-sm font-semibold text-center">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="hover:bg-gray-50 transition duration-300">
-                        <td className="py-4 px-6 border-b">Shiyam </td>
-                        <td className="py-4 px-6 border-b">19</td>
-                        <td className="py-4 px-6 border-b">Male</td>
-                        <td className="py-4 px-6 border-b text-end">Mirpur 15, Dhaka</td>
+                    <tr className="hover:bg-gray-100 transition duration-300">
+                        <td className="p-1 text-xs font-medium text-center border-b">Shiyam Ahmed</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">HPT-19021</td>
+                        <td className="p-1 text-xs font-medium border-b text-end">Mirpur 15, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Sciences</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">class- 9 to 10</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Bangla</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">5K</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">4d</td>
+                        <td className="p-1 text-xs font-medium border-b text-end"> Shemoley, Mirpur-10, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">
+                            <button className="text-[11px] px-1 py-1 bg-red-500 text-white rounded-lg mr-1">Cancel</button>
+                            <button className="text-[11px] px-1 py-1 bg-blue-500 text-white rounded-lg mr-1">Approve</button>
+                        </td>
                     </tr>
-                    <tr className="hover:bg-gray-50 transition duration-300">
-                        <td className="py-4 px-6 border-b">Arif</td>
-                        <td className="py-4 px-6 border-b">17</td>
-                        <td className="py-4 px-6 border-b">Male</td>
-                        <td className="py-4 px-6 border-b text-end">Bagerhat, Khulna</td>
+                    <tr className="hover:bg-gray-100 transition duration-300">
+                        <td className="p-1 text-xs font-medium text-center border-b">Parvej Ahmed Anto</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">HPT-19023</td>
+                        <td className="p-1 text-xs font-medium border-b text-end">Uttora-1, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Commerces</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">class- Ten</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Bangla</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">4K</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">3d</td>
+                        <td className="p-1 text-xs font-medium border-b text-end"> Uttora-2, Ges Fames, Dhaka</td>
+                         <td className="p-1 text-xs font-medium text-center border-b">
+                            <button className="text-[11px] px-1 py-1 bg-red-500 text-white rounded-lg mr-1">Cancel</button>
+                            <button className="text-[11px] px-1 py-1 bg-blue-500 text-white rounded-lg mr-1">Approve</button>
+                        </td>
+                    </tr>
+                    <tr className="hover:bg-gray-100 transition duration-300">
+                        <td className="p-1 text-xs font-medium text-center border-b">Tanjila Akter Shemo</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">HPT-19026</td>
+                        <td className="p-1 text-xs font-medium border-b text-end">Danmondi, Dhaka</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">Sciences</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">class- 11 to 12</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">English</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">7K</td>
+                        <td className="p-1 text-xs font-medium text-center border-b">4d</td>
+                        <td className="p-1 text-xs font-medium border-b text-end"> Fukira Bazar, Mirpur-10, Dhaka</td>
+                         <td className="p-1 text-xs font-medium text-center border-b">
+                            <button className="text-[11px] px-1 py-1 bg-red-500 text-white rounded-lg mr-1">Cancel</button>
+                            <button className="text-[11px] px-1 py-1 bg-blue-500 text-white rounded-lg mr-1">Approve</button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
