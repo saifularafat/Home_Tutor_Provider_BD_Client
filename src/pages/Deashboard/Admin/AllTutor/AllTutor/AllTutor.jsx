4e952612bc982d25f27cc1eb91a7cc95ea3a761a
saifularafat <saifularafat.info@gmail.com>
2024-11-22 22:59:43 +0600

    Admin Deshboard Tutor all table create

--- src/pages/Deashboard/Admin/AllTutor/AllTutor/AllTutor.jsx
+++ src/pages/Deashboard/Admin/AllTutor/AllTutor/AllTutor.jsx
@@ -1,7 +1,23 @@
 const AllTutor = () => {
     return (
-        <div>
-            <h2>All Tutor very soon...</h2>
+
+        <div className="overflow-x-auto ">
+            <table className="min-w-[100%] shadow-md border mx-auto bg-white border-gray-200 md:my-8 my-4">
+                <thead>
+                    <tr className="bg-[#282727] text-white">
+                        <th className="py-2 text-sm text-center border-b"></th>
+                        <th className="py-2 text-sm text-center border-b">Name</th>
+                        <th className="py-2 text-sm text-center border-b">Group/Sub</th>
+                        <th className="py-2 text-sm text-center border-b">C. University</th>
+                        <th className="py-2 text-sm text-center border-b">Experian</th>
+                        <th className="py-2 text-sm text-center border-b">Expert Salary</th>
+                        <th className="py-2 text-sm text-center border-b">C. Student</th>
+                        <th className="py-2 text-sm text-center border-b">Gender</th>
+                        <th className="py-2 text-sm text-center border-b">Address</th>
+                    </tr>
+                </thead>
+                
+            </table>
         </div>
     );
 };
