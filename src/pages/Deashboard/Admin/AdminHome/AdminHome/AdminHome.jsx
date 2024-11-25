bbad131647d26075cff04ace50430d96a084aae5
saifularafat <saifularafat.info@gmail.com>
2024-11-26 01:38:34 +0600

    coaching admin page table data is create

--- src/pages/Deashboard/Admin/AdminHome/AdminHome/AdminHome.jsx
+++ src/pages/Deashboard/Admin/AdminHome/AdminHome/AdminHome.jsx
@@ -1,8 +1,10 @@
+import PageTitleShow from "../../../../../Components/PageTitleShow/PageTitleShow";
 import InfoCart from "./InfoCart";
 
 const AdminHome = () => {
     return (
         <div className="py-10">
+            <PageTitleShow currentPage="Admin Home" />
             <InfoCart />
         </div>
     );
--- src/pages/Deashboard/Admin/CoachingCenterAdmin/CoachingCenterAdmin/CoachingCenterAdmin.jsx
+++ src/pages/Deashboard/Admin/CoachingCenterAdmin/CoachingCenterAdmin/CoachingCenterAdmin.jsx
@@ -1,60 +1,79 @@
 import PageTitleShow from "../../../../../Components/PageTitleShow/PageTitleShow";
+import logo from './../../../../../assets/leaf.jpg'
+import license from './../../../../../assets/premium/tutor3.jpg'
 
 const CoachingCenterAdmin = () => {
     return (
         <div className="overflow-x-auto ">
             <PageTitleShow currentPage="Coaching Center" />
-        <table className="min-w-[100%] shadow-md border mx-auto bg-white border-gray-200 md:my-8 my-4">
-            <thead>
-                <tr className="bg-[#282727] text-white">
-                    <th className="py-2 text-sm text-center border-b"></th>
-                    <th className="py-2 text-sm text-center border-b">Name</th>
-                    <th className="py-2 text-sm text-center border-b">Group/Sub</th>
-                    <th className="py-2 text-sm text-center border-b">C. University</th>
-                    <th className="py-2 text-sm text-center border-b">Experian</th>
-                    <th className="py-2 text-sm text-center border-b">Expert Salary</th>
-                    <th className="py-2 text-sm text-center border-b">C. Student</th>
-                    <th className="py-2 text-sm text-center border-b">Gender</th>
-                    <th className="py-2 text-sm text-center border-b">Address</th>
-                </tr>
-            </thead>
-            <tbody className="">
-                <tr className={`hover:bg-gray-100 transition duration-300`} title="Shiyam Ahmed">
-                    <td className="py-1 text-sm text-center border-b">1</td>
-                    <td className="py-1 text-sm text-center border-b">Shiyam Ahmed</td>
-                    <td className="py-1 text-sm text-center border-b">Sciences</td>
-                    <td className="py-1 text-sm text-center border-b">Dhaka International University</td>
-                    <td className="py-1 text-sm text-center border-b">4 Years</td>
-                    <td className="py-1 text-sm text-center border-b">5 K</td>
-                    <td className="py-1 text-sm text-center border-b"> 4 Student</td>
-                    <td className="py-1 text-sm text-center border-b">Male</td>
-                    <td className="py-1 text-sm text-center border-b"> Kajipara, Mirpur 10, Dhaka</td>
-                </tr>
-                <tr className={`hover:bg-gray-100 transition duration-300`} title="Ejajur Rahman Ejaj">
-                    <td className="py-1 text-sm text-center border-b">2</td>
-                    <td className="py-1 text-sm text-center border-b">Ejajur Rahman Ejaj</td>
-                    <td className="py-1 text-sm text-center border-b">English</td>
-                    <td className="py-1 text-sm text-center border-b">Comilla International University</td>
-                    <td className="py-1 text-sm text-center border-b">1 Years</td>
-                    <td className="py-1 text-sm text-center border-b">3 K</td>
-                    <td className="py-1 text-sm text-center border-b"> 1 Student</td>
-                    <td className="py-1 text-sm text-center border-b">Male</td>
-                    <td className="py-1 text-sm text-center border-b"> Kajipara, Dhanmondi, Dhaka</td>
-                </tr>
-                <tr className={`hover:bg-gray-100 transition duration-300`} title="Shakib All Hasan">
-                    <td className="py-1 text-sm text-center border-b">3</td>
-                    <td className="py-1 text-sm text-center border-b">Shakib All Hasan</td>
-                    <td className="py-1 text-sm text-center border-b">Math</td>
-                    <td className="py-1 text-sm text-center border-b">Jahaggirnagor University Dhaka</td>
-                    <td className="py-1 text-sm text-center border-b">N/A</td>
-                    <td className="py-1 text-sm text-center border-b">5 K</td>
-                    <td className="py-1 text-sm text-center border-b"> 2 Student</td>
-                    <td className="py-1 text-sm text-center border-b">Male</td>
-                    <td className="py-1 text-sm text-center border-b"> Chhar rasta mur, Uttora, Dhaka</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+            <table className="min-w-[100%] shadow-md border mx-auto bg-white border-gray-200 md:my-8 my-4">
+                <thead>
+                    <tr className="bg-[#7a0808] text-white">
+                        <th className="py-2 text-sm text-center border-b"></th>
+                        <th className="py-2 text-sm text-center border-b">Logo</th>
+                        <th className="py-2 text-sm text-center border-b">Name</th>
+                        <th className="py-2 text-sm text-center border-b">Class</th>
+                        <th className="py-2 text-sm text-center border-b">Subject</th>
+                        <th className="py-2 px-2 text-sm text-center border-b">A. Cost</th>
+                        <th className="py-2 px-2 text-sm text-center border-b">C. Student</th>
+                        <th className="py-2 text-sm text-center border-b">S. Date</th>
+                        <th className="py-2 text-sm text-center border-b">Address</th>
+                        <th className="py-2 text-sm text-center border-b">License Photo</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                    <tr className={`hover:bg-gray-100 transition duration-300 border-b`} title="Shiyam Ahmed">
+                        <td className="text-center">1</td>
+                        <td className="pb-1 flex justify-center">
+                            <img src={logo} alt="coaching logo" className="h-12 w-12 rounded-lg object-cover bg-gray-300" />
+                        </td>
+                        <td className="text-sm text-center">Shiyam Ahmed</td>
+                        <td className="text-sm text-center">Nine-Ten</td>
+                        <td className="text-sm text-center">math, english</td>
+                        <td className="px-3 text-sm text-center">5 K</td>
+                        <td className="px-3 text-sm text-center"> 4 S.</td>
+                        <td className="px-3 text-sm text-center"> 01/01/2025</td>
+                        <td className="text-sm text-center"> Kajipara, Mirpur 10, Dhaka</td>
+                        <td className="pb-1 flex justify-center">
+                            <img src={license} alt="coaching logo" className="h-12 w-[72PX] rounded-lg object-cover bg-gray-300" />
+                        </td>
+                    </tr>
+                    <tr className={`hover:bg-gray-100 transition duration-300 border-b`} title="Shiyam Ahmed">
+                        <td className="text-center">2</td>
+                        <td className="pb-1 flex justify-center">
+                            <img src={logo} alt="coaching logo" className="h-12 w-12 rounded-lg object-cover bg-gray-300" />
+                        </td>
+                        <td className="text-sm text-center">Shiyam Ahmed</td>
+                        <td className="text-sm text-center">One-Five</td>
+                        <td className="text-sm text-center">All</td>
+                        <td className="px-3 text-sm text-center">6 K</td>
+                        <td className="px-3 text-sm text-center"> 5 S.</td>
+                        <td className="px-3 text-sm text-center"> 03/02/2025</td>
+                        <td className="text-sm text-center"> Kajipara, Mirpur 10, Dhaka</td>
+                        <td className="pb-1 flex justify-center">
+                            <img src={license} alt="coaching logo" className="h-12 w-[72PX] rounded-lg object-cover bg-gray-300" />
+                        </td>
+                    </tr>
+                    <tr className={`hover:bg-gray-100 transition duration-300 border-b`} title="Shiyam Ahmed">
+                        <td className="text-center">3</td>
+                        <td className="pb-1 flex justify-center">
+                            <img src={logo} alt="coaching logo" className="h-12 w-12 rounded-lg object-cover bg-gray-300" />
+                        </td>
+                        <td className="text-sm text-center">Shiyam Ahmed</td>
+                        <td className="text-sm text-center">Seven-Eight</td>
+                        <td className="text-sm text-center">Math, English, Science</td>
+                        <td className="px-3 text-sm text-center">4 K</td>
+                        <td className="px-3 text-sm text-center"> 3 S.</td>
+                        <td className="px-3 text-sm text-center"> 01/02/2025</td>
+                        <td className="text-sm text-center"> Kajipara, Mirpur 10, Dhaka</td>
+                        <td className="pb-1 flex justify-center">
+                            <img src={license} alt="coaching logo" className="h-12 w-[72PX] rounded-lg object-cover bg-gray-300" />
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
     );
 };
 
