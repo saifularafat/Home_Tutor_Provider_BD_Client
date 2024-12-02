6f53072e75cc2faeab133f95b26f78392fc18fa0
saifularafat <saifularafat.info@gmail.com>
2024-12-02 22:02:19 +0600

    Admin home page a marquery add

--- src/pages/Deashboard/Admin/AdminHome/AdminHome/AdminHome.jsx
+++ src/pages/Deashboard/Admin/AdminHome/AdminHome/AdminHome.jsx
@@ -1,3 +1,4 @@
+import MarqueeSliderPage from "../../../../../Components/MarqueeSliderPage/MarqureeSliderPage";
 import PageTitleShow from "../../../../../Components/PageTitleShow/PageTitleShow";
 import InfoCart from "./InfoCart";
 
@@ -6,6 +7,14 @@ const AdminHome = () => {
         <div className="py-10">
             <PageTitleShow currentPage="Admin Home" />
             <InfoCart />
+
+          <div className="mx-5">
+          <MarqueeSliderPage
+                notice="Notice"
+                noticeBG='bg-orange-600'
+                marquessText="আপনার প্রফাইল আপডেট করুন। প্রোফাইল আপডেট না করলে আপনি কোন টিউশনে আবেদন করতে পারবেন না এবং আপনার প্রফাইল এর কোন ফিচার দেখতে পারবেন না। তাই প্রোফাইল আপডেট করে আমাদের টিউশনে অ্যাপ্লাই করুন।"
+            />
+          </div>
         </div>
     );
 };
