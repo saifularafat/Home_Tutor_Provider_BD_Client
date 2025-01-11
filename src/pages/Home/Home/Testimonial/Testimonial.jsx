207f8c1b6ce5cd70b9e49e722ba2fa8ab6aa29c8
saifularafat <saifularafat.info@gmail.com>
2025-01-12 02:19:35 +0600

    testimonial section is a modify and user info is change

--- src/pages/Home/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Home/Testimonial/Testimonial.jsx
@@ -3,12 +3,18 @@ import SectionTitle from "../../../../Components/SectionTitle/SectionTitle";
 import { useAllRatings } from "../../../../api/useAllRatings";
 import Loading from "../../../../Components/Loading/Loading";
 import { array } from "../../../../../data";
+import { useAllUsers } from "../../../../api/useAllUsers";
+import RatingStar from "../../../../Share/Reviews/RatingStar";
 
 const Testimonial = () => {
     const [currentSlider, setCurrentSlider] = useState(0);
+    const [usersWithRatings, setUsersWithRatings] = useState([]);
 
     const [payload, refetch, isLoading] = useAllRatings();
-    const { rating = [], pagination = {} } = payload || { rating: [], pagination: {} };
+    const { rating = [] } = payload || { rating: [] };
+
+    const [usersData] = useAllUsers();
+    const { users } = usersData || {};
 
     const sortedRatings = [...rating].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
@@ -24,6 +30,21 @@ const Testimonial = () => {
             currentSlider === testimonials.length - 1 ? 0 : currentSlider + 1
         );
 
+    // console.log("usersWithRatings =>", usersWithRatings);
+    const getUserInfo = (userEmail) => {
+        return users?.find(user => user.email === userEmail);
+    };
+
+    useEffect(() => {
+        if (rating && users) {
+            const matchedUsersWithRatings = rating.map((review) => {
+                const user = getUserInfo(review.userEmail);
+                return user ? { ...review, userInfo: user } : null;
+            }).filter(review => review !== null);
+            setUsersWithRatings(matchedUsersWithRatings);
+        }
+    }, [rating, users]);
+
     useEffect(() => {
         const intervalId = setInterval(nextSlider, 5000);
         return () => clearInterval(intervalId);
@@ -34,17 +55,17 @@ const Testimonial = () => {
 
 
     const renderStars = (ratingValue) => {
-        const filledStars = Math.min(Math.max(ratingValue, 0), 5); 
-        const emptyStars = 5 - filledStars; 
+        const filledStars = Math.min(Math.max(ratingValue, 0), 5);
+        const emptyStars = 5 - filledStars;
 
         const filledStarIcons = [...Array(filledStars)].map((_, idx) => (
-            <svg key={`filled-${idx}`} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5 h-5 text-yellow-400" viewBox="0 0 24 24">
-                <path d="M12 .587l3.668 7.431 8.2 1.189-5.93 5.784 1.398 8.14-7.536-3.96-7.536 3.96 1.398-8.14-5.93-5.784 8.2-1.189z"/>
+            <svg key={`filled-${idx}`} xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-5 h-5 text-red-400" viewBox="0 0 24 24">
+                <path d="M12 .587l3.668 7.431 8.2 1.189-5.93 5.784 1.398 8.14-7.536-3.96-7.536 3.96 1.398-8.14-5.93-5.784 8.2-1.189z" />
             </svg>
         ));
         const emptyStarIcons = [...Array(emptyStars)].map((_, idx) => (
             <svg key={`empty-${idx}`} xmlns="http://www.w3.org/2000/svg" fill="none" className="w-5 h-5 text-gray-300" viewBox="0 0 24 24">
-                <path d="M12 .587l3.668 7.431 8.2 1.189-5.93 5.784 1.398 8.14-7.536-3.96-7.536 3.96 1.398-8.14-5.93-5.784 8.2-1.189z"/>
+                <path d="M12 .587l3.668 7.431 8.2 1.189-5.93 5.784 1.398 8.14-7.536-3.96-7.536 3.96 1.398-8.14-5.93-5.784 8.2-1.189z" />
             </svg>
         ));
         return [...filledStarIcons, ...emptyStarIcons];
@@ -53,11 +74,11 @@ const Testimonial = () => {
     if (isLoading) {
         return <Loading />;
     }
-    
+
     return (
         <>
             <SectionTitle sectionName="Testimonial" />
-            <div className="container md:min-w-[350px] mx-auto md:h-[280px] flex flex-row items-center overflow-hidden md:gap-5 gap-3 lg:gap-10 px-6 md:px-6 lg:px-5 md:pb-10 pb-7 md:pt-8 pt-4">
+            <div className="container md:min-w-[350px] mx-auto md:h-[280px] my-5 flex flex-row items-center overflow-hidden md:gap-5 gap-3 lg:gap-10 px-6 md:px-6 lg:px-5 md:pb-10 pb-7 md:pt-8 pt-4">
                 <div className="relative overflow-hidden">
                     <div className="absolute w-full h-full flex items-center justify-between z-50 md:px-5 ">
                         {/* arrow left */}
@@ -75,10 +96,9 @@ const Testimonial = () => {
                     <div
                         className="ease-linear duration-300 flex"
                         style={{ transform: `translateX(-${currentSlider * (isSmallScreen ? 100 : 50)}%)` }} >
-                        {/* sliders */}
-                        {testimonials.map((rating) => (
+                        {usersWithRatings.map((rating) => (
                             <div key={rating?._id} className="md:p-3 p-1 min-w-full md:min-w-[50%]">
-                                <div className="h-full md:p-8 p-3 rounded shadow-[0px_4px_12px_rgba(0,0,0,0.1)]">
+                                <div className="h-full rounded-lg bg-slate-200 md:p-8 p-3 shadow-[0px_4px_12px_rgba(0,0,0,0.1)]">
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="currentColor"
@@ -87,25 +107,26 @@ const Testimonial = () => {
                                     >
                                         <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                                     </svg>
-                                    <p className="leading-relaxed md:mb-6 mb-3 text-gray-500">
+                                    <p className="leading-relaxed md:mb-6 mb-3 text-gray-700">
                                         {rating.review || rating.testimonialDescription}
                                     </p>
                                     <div className="flex space-x-1 mb-4">
-                                        {renderStars(rating?.rating || 0)} 
+                                        {renderStars(rating?.rating || 0)}
+                                        {/* <RatingStar id={rating._id} star={rating.rating} /> */}
                                     </div>
-                                    <a className="inline-flex items-center">
+                                    <a className="inline-flex items-center bottom-0">
                                         <img
                                             className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-                                            src={`https://source.unsplash.com/200x200/?person`}
+                                            src={rating.userInfo?.image || "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop"}
                                             alt="user photo"
                                         />
                                         <span className="flex-grow flex flex-col md:pl-4 pl-3">
                                             <span className="font-medium text-gray-900">
-                                                {rating.guardianEmail || rating.name}
+                                                {rating?.userInfo?.name || 'user Name'}
                                             </span>
-                                            <div className="flex  items-center justify-between">
+                                            <div className="flex items-center justify-between">
                                                 <span className="text-gray-500 text-sm">
-                                                    {rating.designation || "User"}
+                                                    {/* {rating.designation || "User"} */}
                                                 </span>
                                                 <span className="font-normal text-xs">
                                                     {new Date(rating?.createdAt).toLocaleDateString()}{" "}
@@ -114,6 +135,7 @@ const Testimonial = () => {
                                             </div>
                                         </span>
                                     </a>
+                                    {/* )} */}
                                 </div>
                             </div>
                         ))}
