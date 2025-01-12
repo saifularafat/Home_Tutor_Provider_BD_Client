5f11c5fd236f33b8bde5546e1b4540c65e4ab374
saifularafat <saifularafat.info@gmail.com>
2025-01-13 01:03:10 +0600

    notice form is change

--- src/pages/Deashboard/Admin/CreateNotice/CreateNotice/CreateNotice.jsx
+++ src/pages/Deashboard/Admin/CreateNotice/CreateNotice/CreateNotice.jsx
@@ -1,9 +1,8 @@
 import { useForm } from "react-hook-form";
-import SearchDropdown from "../../../../../Components/SearchInputFuntion/SearchDropdown";
 import PreferableClassOptions from "../../../../../Helpers/PreferableClass";
-import { useState } from "react";
-import WeekDay from "../../../../../Helpers/WeekDay";
 import PageTitleShow from "../../../../../Components/PageTitleShow/PageTitleShow";
+import SearchDropDownField from "../../../../../Components/SearchDropDownFiled/SearchDropDownField";
+import { FixedTime, JobCategory, JobSalary, PerWeek } from "../../../../../Helpers/TuitionJobCreate";
 
 const CreateNotice = () => {
     const {
@@ -11,108 +10,81 @@ const CreateNotice = () => {
         handleSubmit,
         reset,
         setValue,
+        watch,
         formState: { errors }
     } = useForm()
 
-    const [preferableClass, setPreferableClass] = useState('');
-    const [weekDay, setWeekDay] = useState('');
-
+    const jobCategory = watch("jobCategory");
+    const className = watch("className");
+    const jobSalary = watch("jobSalary");
+    const perWeek = watch("perWeek");
+    const fixedTime = watch("fixedTime");
 
     const onSubmit = data => {
         const noticeInfo = {
-            locationName: data.locationName,
-            jobName: data.jobName,
-            salary: data.salary,
-            time: data.time,
-            preferableClass: preferableClass,
-            weekDay: weekDay,
+            jobLocation: data.jobLocation,
+            jobCategory: jobCategory,
+            className: className,
+            jobSalary: jobSalary,
+            perWeek: perWeek,
+            jobTime: fixedTime,
         }
+        reset()
         console.log(noticeInfo);
     }
-
-    // Function to update the selected Programming langues
-    const handlePreferableClass = (option) => {
-        setPreferableClass(option);
-        setValue('preferableClass', option);
-    };
-
-    const handleWeekDay= (option) => {
-        setWeekDay(option);
-        setValue('weekDay', option);
-    };
     return (
-        <div className="mt-5">
-              <PageTitleShow currentPage="Notice Create |" />
+        <div className="my-8">
+            <PageTitleShow currentPage="Notice Create |" />
             <form
                 onSubmit={handleSubmit(onSubmit)}
                 className='mx-auto max-w-screen-lg border-2 bg-white rounded-xl p-3 md:p-6 shadow-lg'
             >
                 <h1 className='md:text-3xl text-xl font-semibold my-2 md:my-4 text-center text-slate-700'>Create Notice</h1>
                 <div className='grid md:grid-cols-3 grid-cols-1 gap-4'>
-                    <div className='w-full mb-2 form-control'>
-                        <label className='label'>
-                            <span className='font-semibold label-text'>Location Name</span>
+                    <div className="space-y-2 text-left">
+                        <label className="block text-slate-700 font-medium">
+                            <span className="font-semibold text-slate-800 tracking-wider">Job Location*</span>
                         </label>
                         <input
                             type='text'
                             placeholder='Location Name'
-                            {...register('locationName', { required: true })}
+                            {...register('jobLocation', { required: true })}
                             className='bg-transparent input border border-sky-300 rounded-lg outline-sky-600 px-4 py-3 w-full placeholder:text-sm placeholder:tracking-wider text-sm'
                         />
+                        {errors.jobLocation && (
+                            <p className="text-red-500 text-sm">{errors.jobLocation.message}</p>
+                        )}
                     </div>
-                    <div className='w-full mb-2 form-control'>
-                        <label className='label'>
-                            <span className='font-semibold label-text'>Job Name</span>
-                        </label>
-                        <input
-                            type='text'
-                            placeholder='Job Name'
-                            {...register('jobName', { required: true })}
-                            className='bg-transparent input border border-sky-300 rounded-lg outline-sky-600 px-4 py-3 w-full placeholder:text-sm placeholder:tracking-wider text-smcapitalize'
-                        />
-                    </div>
-                    <div className='w-full mb-2 form-control'>
-                        <label className='label'>
-                            <span className='font-semibold label-text'>Class </span>
-                        </label>
-                        <SearchDropdown
-                            options={PreferableClassOptions}
-                            selectedValue={preferableClass}
-                            onSelect={handlePreferableClass} 
-                        />
-                    </div>
-                    <div className='w-full form-control '>
-                        <label className='label'>
-                            <span className='font-semibold label-text'>salary</span>
-                        </label>
-                        <input
-                            type='text'
-                            {...register('salary', { required: true })}
-                            placeholder='Salary'
-                            className='bg-transparent input border border-sky-300 rounded-lg outline-sky-600 px-4 py-3 w-full placeholder:text-sm placeholder:tracking-wider text-sm'
-                        />
-                    </div>
-                    <div className='w-full form-control '>
-                        <label className='label'>
-                            <span className='font-semibold label-text'>Week Day</span>
-                        </label>
-                        <SearchDropdown
-                            options={WeekDay}
-                            selectedValue={weekDay}
-                            onSelect={handleWeekDay} 
-                        />
-                    </div>
-                    <div className='w-full form-control '>
-                        <label className='label'>
-                            <span className='font-semibold label-text'>Time</span>
-                        </label>
-                        <input
-                            type='Time'
-                            {...register('time', { required: true })}
-                            placeholder='time'
-                            className='bg-transparent input border border-sky-300 rounded-lg outline-sky-600 px-4 py-3 w-full placeholder:text-sm placeholder:tracking-wider text-sm'
-                        />
-                    </div>
+                    <SearchDropDownField
+                        label="Job Category *"
+                        options={JobCategory}
+                        selectedValue={jobCategory}
+                        setValue={(value) => setValue("jobCategory", value)}
+                    />
+                    <SearchDropDownField
+                        label="Class Name *"
+                        options={PreferableClassOptions}
+                        selectedValue={className}
+                        setValue={(value) => setValue("className", value)}
+                    />
+                    <SearchDropDownField
+                        label="Job Salary *"
+                        options={JobSalary}
+                        selectedValue={jobSalary}
+                        setValue={(value) => setValue("jobSalary", value)}
+                    />
+                    <SearchDropDownField
+                        label="Per Week *"
+                        options={PerWeek}
+                        selectedValue={perWeek}
+                        setValue={(value) => setValue("perWeek", value)}
+                    />
+                    <SearchDropDownField
+                        label="Time *"
+                        options={FixedTime}
+                        selectedValue={fixedTime}
+                        setValue={(value) => setValue("fixedTime", value)}
+                    />
                 </div>
                 <div className='flex justify-center w-full mt-5'>
                     <input
