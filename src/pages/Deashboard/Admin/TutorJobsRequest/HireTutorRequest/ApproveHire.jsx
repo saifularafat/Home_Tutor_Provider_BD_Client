5ba7dbbfd9cb756da93b15c3ed79d0c4fe77c309
saifularafat <saifularafat.info@gmail.com>
2025-01-13 21:36:38 +0600

    hire tutor admin page is dynamic approve hire is done now

--- src/pages/Deashboard/Admin/TutorJobsRequest/HireTutorRequest/ApproveHire.jsx
+++ src/pages/Deashboard/Admin/TutorJobsRequest/HireTutorRequest/ApproveHire.jsx
@@ -10,37 +10,103 @@ const ApproveHire = ({ approveHire, refetch, isLoading }) => {
     return (
         <div className="overflow-x-auto">
             <table className="min-w-[100%] shadow-md border mx-auto border-gray-100">
+                {/* head */}
                 <thead>
                     <tr className="bg-blue-500 text-white">
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Tutor Name</th>
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">T. Id</th>
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">T. Area</th>
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Subject</th>
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Salary</th>
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">Parent Name</th>
-                        <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">P. Id</th>
-                        <th className="py-2 pr-2 border-b capitalize text-sm font-semibold text-center">P. Area</th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            #
+                        </th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            <p className="text-xs"> Tutor Name</p>
+                            <p className="text-[10px] leading-3"> Tutor Id</p>
+                        </th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            <p className="text-xs"> Tutor Area</p>
+                            <p className="text-[10px] leading-3"> Tutor Phone</p>
+                        </th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            <p className="text-xs">Job Subject</p>
+                            <p className="text-[10px] leading-3">Job Category</p>
+                        </th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            <p className="text-xs"> Job Salary</p>
+                            <p className="text-[10px] leading-3"> Tuition Start</p>
+                        </th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            <p className="text-xs"> Guardian Name</p>
+                            <p className="text-[10px] leading-3"> Guardian Id</p>
+                        </th>
+                        <th className="py-2 pl-1 capitalize text-center border-b font-medium">
+                            <p className="text-xs"> Guardian Area</p>
+                            <p className="text-[10px] leading-3"> Guardian Phone</p>
+                        </th>
                         <th className="py-2 pl-2 capitalize text-center border-b text-sm font-semibold">A. Date</th>
                         <th className="py-2 pr-2 border-b capitalize text-sm font-semibold text-center">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        approveHire.map((hire) =>
+                        approveHire.map((hire, inx) =>
                             <tr
                                 key={hire?._id}
-                                className="hover:bg-gray-100 transition duration-300">
-                                <td className="p-1 text-xs font-medium text-center border-b">{hire?.tutorName || 'Tutor Name'}</td>
-                                <td className="p-1 text-xs font-medium text-center border-b">{hire?.tutorId || "HTP-0000"}</td>
-                                <td className="p-1 text-xs font-medium border-b text-end">{hire?.tutorAddress || 'Dhaka, Bangladesh'}</td>
-                                <td className="p-1 text-xs font-medium text-center border-b">{hire?.jobSubject || 'Sciences'}</td>
-                                <td className="p-1 text-xs font-medium text-center border-b">{hire?.jobClass || 'class-N/A'}</td>
-                                <td className="p-1 text-xs font-medium text-center border-b">{hire?.jobMedium || 'Bangla'}</td>
-                                <td className="p-1 text-xs font-medium text-center border-b">{hire?.guardianId || 'P-12450'}</td>
-                                <td className="p-1 text-xs font-medium border-b text-end"> {hire?.guardianAddress || 'Dhaka, Bangladesh'}</td>
-                                <td className="p-1 text-xs font-medium text-center border-b">
-                                    {new Date(hire?.createdAt).toLocaleDateString()}{" "}
-                                    {new Date(hire?.createdAt).toLocaleTimeString()}
+                                className="hover:bg-gray-100 transition duration-300 border-slate-200 border-b-[2px]"
+                            >
+                                <td className="py-2 font-semibold">{inx + 1}</td>
+                                <td className="pl-2 py-2">
+                                     <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm">{hire?.tutorName || 'Tutor Name'}</div>
+                                            <div className="font-medium text-xs opacity-60">{hire?.tutorId || "HTP-0000"}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2">
+                                     <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm">{hire?.tutorAddress || 'Dhaka, Bangladesh'}</div>
+                                            <div className="font-medium text-xs opacity-70">{hire?.tutorPhone || "+8801000000000"}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2">
+                                     <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm">{hire?.JobSubject || 'Sciences'}</div>
+                                            <div className="font-medium text-xs opacity-60">{hire?.JobCategory || "Home"}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2">
+                                     <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm">{hire?.tuitionSalary || '0000'}TK</div>
+                                            <div className="font-medium text-xs opacity-60">Start {hire?.tuitionStartMonth || "MM"}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2">
+                                     <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm">{hire?.guardianName || 'guardian name'}</div>
+                                            <div className="font-medium text-xs opacity-50">{hire?.guardianId || 'P-00000'}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2">
+                                    <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm">{hire?.guardianAddress || 'Dhaka, Bangladesh'}</div>
+                                            <div className="font-medium text-xs opacity-70">{hire?.guardianPhone || "+8801888888888"}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2">
+                                    <div className="flex items-center text-center justify-center">
+                                        <div>
+                                            <div className="font-medium text-sm"> {new Date(hire?.createdAt).toLocaleDateString()}</div>
+                                            <div className="font-medium text-xs opacity-70">{new Date(hire?.createdAt).toLocaleTimeString()}</div>
+                                        </div>
+                                    </div>
                                 </td>
                                 <td className="p-1 text-xs font-medium text-center border-b">
                                     <button
@@ -48,14 +114,13 @@ const ApproveHire = ({ approveHire, refetch, isLoading }) => {
                                         //     "Tuition Job",
                                         //     "Your Tuition Job"
                                         // )}
-                                        className="text-xs px-1 py-1 bg-red-400 hover:bg-red-600 hover:underline text-white rounded-lg transition-all duration-200">Cancel</button>
+                                        className="text-xs px-1 py-2 bg-red-400 hover:bg-red-600 hover:underline text-white rounded-lg transition-all duration-200">Cancel</button>
                                 </td>
                             </tr>
-                        )
-                    }
+                        )}
                 </tbody>
             </table>
-        </div>
+        </div >
     );
 };
 
